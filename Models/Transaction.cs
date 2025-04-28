e3f94e98064ad004c64f8b4c7ef34408f5500245
彭建欽 <S376079@sinyi.com.tw>
2025-04-28 14:19:45 +0800

    欄位調整

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace YourProjectNamespace.Models
 {
     public class Transaction
     {
+        public Transaction()
+        {
+            Date = DateTime.Today;
+        }
+
         public int Id { get; set; }
         public string Category { get; set; }
         public decimal Money { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public string Description { get; set; }
     }
